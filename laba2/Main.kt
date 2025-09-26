60a8fcd649253c008eb9a3e7c363c4908a4aca1f
gpdspeed <grgrv.egr@yandex.ru>
2025-09-26 08:14:16 +0700

    Update Main.kt

--- laba2/Main.kt
+++ laba2/Main.kt
@@ -7,26 +7,32 @@ fun main(){
         Human("Виктор", "Сидоров", "Алексеевич", 21, 6),
         Human("Ольга", "Васильева", "Сергеевна", 19, 3)
     )
+
     val driver = Driver("Егор", "Григорьев", "Васильевич", 19, 8)
     val simulationTime = 5
     val threads = mutableListOf<Thread>()
-    for (person in people) {
+    for ((index, person) in people.withIndex()) {
         val thread = thread {
+            println("Запущен поток для ${person.name}")
             repeat(simulationTime) {
                 person.move()
-                Thread.sleep(1000)
+                Thread.sleep(2000)
             }
+            println("Поток для ${person.name} завершен")
         }
         threads.add(thread)
     }
 
     val driverThread = thread {
+        println("Запущен поток для водителя ${driver.name}")
         repeat(simulationTime) {
             driver.move()
-            Thread.sleep(2000)
+            Thread.sleep(1000)
         }
+        println("Поток для водителя завершен")
     }
     threads.add(driverThread)
+    println("\nОжидание завершения потоков...")
     threads.forEach { it.join() }
     println("\nФинальные позиции:")
     for (person in people) {
